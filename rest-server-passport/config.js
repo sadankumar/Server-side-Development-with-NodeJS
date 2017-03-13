9f28998d44e000f2045fbba9eef9a618766b8a60
Espen Reksten Berentsen <esbe@kurs.aai.no>
2017-03-13 11:12:17 +0100

    exercises

--- rest-server-passport/config.js
+++ rest-server-passport/config.js
@@ -1,4 +1,9 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientID: '481272272262129',
+        clientSecret: '0ead755c65291ff807d196e3acbf76d4',
+        callbackURL: 'https://localhost:3443/users/facebook/callback'
+    }
 }
